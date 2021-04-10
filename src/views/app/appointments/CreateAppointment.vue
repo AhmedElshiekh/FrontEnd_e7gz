e7ffb33649157eb71390ca6b4238d1252a3a7925
Yossef Alatter <63886402+yossefAlatter@users.noreply.github.com>
2021-04-10 18:39:54 +0200

    filter start

--- src/views/app/appointments/CreateAppointment.vue
+++ src/views/app/appointments/CreateAppointment.vue
@@ -33,28 +33,65 @@
     <!-- start -->
     <div class="filter-con">
       <input type="text" placeholder="Enter doctor name ...">
-      <button>Filter</button>
+      <button @click="openFilterBox()">Filter</button>
     </div>
     <!-- start -->
+
+    <!-- filter box -->
+    <!-- start -->
+    <div class="filter-box" style="display: none">
+      <div class="filter-group">
+        <h5>Specailist</h5>
+      <label for=""></label>
+      <select name="" id="specailists">
+        <option value="">Select</option>
+        <option :value="specailist.key" v-for="specailist in specailists" :key="specailist.key">{{ specailist.name }}</option>
+      </select>
+      </div>
+      <div class="filter-group">
+        <h5>Location</h5>
+      <div> 
+        <label for="" style="margin-right: 14px">Governate: </label>
+        <select name="" id="governorates" v-for="governorate in governorates" :key="governorate.key" @change="selectCity()">
+          <option value="">Select</option>
+          <option :value="governorate.name">{{ governorate.name }}</option> 
+        </select>
+      </div>
+      <div>
+        <label for="" style="margin-right: 55px">City: </label>
+        <select name="" id="cities" disabled>
+          <option value="">select</option>
+          <option :value="city.name" v-for="city in cities" :key="city.key">{{ city.name }}</option>
+        </select>
+      </div>
+      </div>
+      <div class="filter-group">
+        <h5>Date</h5>
+      <label for=""></label>
+      <input type="date" name="" id="date">
+      </div>
+      <button @click="makeFilter()">Filter</button>
+    </div>
+    <!-- end -->
 
     <!-- appiontments cards -->
     <!-- start -->
     <div class="appointments-con">
-      <div class="appointment-card" v-for="num in nums" :key="num">
+      <div class="appointment-card" v-for="appoi in todayAppointments" :key="appoi.key">
         <div class="top-side">
           <div class="left-side">
             <div class="img-con">
               <img src="img/avatars/6.jpg" alt="avatar">
             </div>
             <div class="img-info">
-              <h4>Ahmed Zaki</h4>
-              <h5><small>nose and ear specialist</small></h5>
-              <span>Egypt . Cairo . Nasr City</span><br>
+              <h4>{{ appoi.doctorName }}</h4>
+              <h5><small>{{appoi.department?specailists[appoi.department].name:"no specailist"}}</small></h5>
+              <span>{{ appoi.country }} . {{appoi.governorate}} . {{ appoi.city }}</span><br>
               <small>11/12/2020</small>
             </div>
           </div>
           <div class="right-side">
-            <span>11:30:00</span>
+            <span>{{ appoi.timeFrom }} => {{ appoi.timeFrom }}</span>
           </div>
         </div>
         <div class="bottom-side">
@@ -90,62 +127,148 @@ export default {
         dismissSecs: 7,
         dismissCountDown: 0,
         showDismissibleAlert: false,
-        nums:[1,0,0,0,0,0,0,0,1,2,5,2,5,2,35,511,51,5,55,5,5,5,5]
+
+        //== data catch variables ==//
+        //== start ==//
+        apiUrl: "https://api.aisent.net/api",
+        nums:[1,2,3,4,5,6],
+        todayAppointments: {},
+        specailists: {},
+        governorates: {},
+        cities: {},
+        counter: 4 ,
+        //== end ==//
     }
   },
   methods: {
+
+    //== my functions==//
+    //== start ==//
+    catchTodayData(){
+      let self = this ;
+      axios
+        .all([
+          axios.post(this.apiUrl + '/filterTimes?token=' + localStorage.getItem("api_token"),{}),
+          axios.get(this.apiUrl + '/specialists?token=' + localStorage.getItem("api_token"),{}),
+          axios.get(this.apiUrl + '/governorates?token=' + localStorage.getItem("api_token"),{})
+        ])
+        .then(
+          axios.spread((...responses) => {
+            self.todayAppointments = responses[0].data.data ;
+            self.specailists = responses[1].data.data ;
+            self.governorates = responses[2].data.data;
+          }
+        ))
+        .catch(err => console.log(err));
+    },
+
+    openFilterBox(){
+      var filterBox = document.querySelector(".filter-box");
+      if(this.counter%2 == 0){
+        filterBox.style.display = "flex";
+      }else{
+        filterBox.style.display = "none";
+      }
+      this.counter ++ ;
+      console.log(this.counter);
+    },
+    selectCity(){
+      var selectedGover = document.getElementById("governorates").value ;
+      if(selectedGover){
+        document.getElementById("cities").removeAttribute("disabled");
+        let self = this ;
+        axios
+          .get(this.apiUrl + `/citiesFromGov/${selectedGover}?token=` + localStorage.getItem("api_token"))
+          .then(res => self.cities = res.data.data);
+      }
+    },
+    makeFilter(){
+      var selectedGover = document.getElementById("governorates").value;
+      var selectedCity = document.getElementById("cities").value;
+      var selectedspecailist = document.getElementById("specailists").value;
+      var selectedDate = document.getElementById("date").value;
+      console.log(selectedGover)
+      console.log(selectedCity)
+      console.log(selectedspecailist)
+      console.log(selectedDate)
+      let self = this ;
+      axios
+        .post(this.apiUrl + '/filterTimes?token=' + localStorage.getItem("api_token"),{
+          data:{
+            date: selectedDate,
+            user_id: 70,
+            department: selectedspecailist,
+            governorate: selectedGover,
+            city: selectedCity
+          }
+        })
+        .then(res =>{
+          self.todayAppointments = res.data.data
+          console.log(res.data.data)
+        } )
+        .catch(err => console.log(err));
+      console.log(self.todayAppointments)
+    },
+    //== end ==//
+
     goBack() {
       this.$router.go(-1)
       // this.$router.replace({path: '/users'})
     },
-    store() {
-        let self = this;
-        axios.post(  this.$apiAdress + '/api/appointments?token=' + localStorage.getItem("api_token"),
-          {
-            name: self.appointment.name,
-          }
-        )
-        .then(function (response) {
-            self.note = {
-              title: '',
-              content: '',
-              applies_to_date: '',
-              status_id: null,
-              note_type: '',
-            };
-            self.message = 'Successfully created appointment.';
-            self.showAlert();
-        }).catch(function (error) {
-            if(error.response.data.data.message == 'The given data was invalid.'){
-              self.message = '';
-              for (let key in error.response.data.data.errors) {
-                if (error.response.data.data.errors.hasOwnProperty(key)) {
-                  self.message += error.response.data.data.errors[key][0] + '  ';
-                }
-              }
-              self.showAlert();
-            }else{
-              console.log(error);
-              self.$router.push({ path: 'login' }); 
-            }
-        });
-    },
-    countDownChanged (dismissCountDown) {
-      this.dismissCountDown = dismissCountDown
-    },
-    showAlert () {
-      this.dismissCountDown = this.dismissSecs
-    },
+    // store() {
+    //     let self = this;
+    //     axios.post(  this.$apiAdress + '/api/appointments?token=' + localStorage.getItem("api_token"),
+    //       {
+    //         name: self.appointment.name,
+    //       }
+    //     )
+    //     .then(function (response) {
+    //         self.note = {
+    //           title: '',
+    //           content: '',
+    //           applies_to_date: '',
+    //           status_id: null,
+    //           note_type: '',
+    //         };
+    //         self.message = 'Successfully created appointment.';
+    //         self.showAlert();
+    //     }).catch(function (error) {
+    //         if(error.response.data.data.message == 'The given data was invalid.'){
+    //           self.message = '';
+    //           for (let key in error.response.data.data.errors) {
+    //             if (error.response.data.data.errors.hasOwnProperty(key)) {
+    //               self.message += error.response.data.data.errors[key][0] + '  ';
+    //             }
+    //           }
+    //           self.showAlert();
+    //         }else{
+    //           console.log(error);
+    //           self.$router.push({ path: 'login' }); 
+    //         }
+    //     });
+    // },
+    // countDownChanged (dismissCountDown) {
+    //   this.dismissCountDown = dismissCountDown
+    // },
+    // showAlert () {
+    //   this.dismissCountDown = this.dismissSecs
+    // },
   },
   mounted: function(){
-    let self = this;
-    axios.get(  this.$apiAdress + '/api/appointments/create?token=' + localStorage.getItem("api_token"))
-    .then(function (response) {
-        self.statuses = response.data.data;
-    }).catch(function (error) {
-        console.log(error);
-        self.$router.push({ path: 'login' });
-    });
+
+    //== catching data first ==//
+    //== start ==//
+    this.catchTodayData();
+    ///== end ==//
+
+    // let self = this;
+    // axios.get(  this.$apiAdress + '/api/appointments/create?token=' + localStorage.getItem("api_token"))
+    // .then(function (response) {
+    //     self.statuses = response.data.data;
+    // }).catch(function (error) {
+    //     console.log(error);
+    //     self.$router.push({ path: 'login' });
+    // });
   }
 }
 
@@ -166,6 +289,7 @@ export default {
     padding-left: 15px;
     font-size: 15px;
     padding: 12px;
+    
   }
   button{
     background-color: #3C4B64;
@@ -181,6 +305,25 @@ export default {
           opacity: 0.8;
     }
   }
+}
+//== end ==//
+
+//== filter box style ==//
+//== start ==//
+.filter-box{
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 20px;
+  .filter-group{
+    margin-right: 70px;
+    select{
+      width: 180px;
+      border: none;
+      outline: none;
+      padding: 5px;
+    }
+  }
+  
 }
 //== end ==//
 
